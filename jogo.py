dbc995fb67dd3462feced6a8fa09c08273fa8f03
André Mikael <andremikael93@gmail.com>
2025-09-18 00:02:38 -0300

    feat: add dino

--- jogo.py
+++ jogo.py
@@ -3,23 +3,34 @@ import time
 import random
 
 pygame.init()
-tela_larg, tela_alt = 600, 400
+
+#tela
+tela_larg, tela_alt = 700, 500
 tela = pygame.display.set_mode((tela_larg, tela_alt))
-pygame.display.set_caption("Breakout")
+pygame.display.set_caption("Dinossauro")
 
-fonte = pygame.font.SysFont("Calibri", 15)
-pontuacao = fonte.render("PONTOS:", True, (160, 32, 240))
+#coloca texto
+fonte = pygame.font.SysFont("Arial", 15)
+distancia = fonte.render("Distância:", True, (160, 32, 240))
 
+#coloca sprite do dinossauro
+dinossauro = pygame.image.load("imagens/dino.png.png")
+dinossauro = pygame.transform.scale(dinossauro, (50, 50))
 
+dino_rect = dinossauro.get_rect(topleft=(100, tela_alt - 100))
+
+#roda o jogo
 jogando = True
 while jogando:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             jogando = False
+        
 
     fundo = (255, 255, 255)
     tela.fill(fundo)
     tela.blit(pontuacao, (2, 2)) #posição na tela
+    tela.blit(dinossauro, dino_rect)
     pygame.display.flip()
 
 
